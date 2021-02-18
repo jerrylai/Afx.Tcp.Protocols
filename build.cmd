807e60ee1b122b5d5ce2917c62aa204272829290
jerrylai <jerrylai@aliyun.com>
2021-02-18 14:56:51 +0800

    9.1.0

--- build.cmd
+++ build.cmd
@@ -3,7 +3,8 @@ set Build="%SYSTEMDRIVE%\Program Files (x86)\Microsoft Visual Studio\2019\Enterp
 if exist publish rd /s /q publish
 %Build% "NET20/Afx.Tcp.Protocols/Afx.Tcp.Protocols.csproj" /t:Rebuild /p:Configuration=Release
 %Build% "NET40/Afx.Tcp.Protocols/Afx.Tcp.Protocols.csproj" /t:Rebuild /p:Configuration=Release
-dotnet build "NETStandard2.0/Afx.Tcp.Protocols/Afx.Tcp.Protocols.csproj" -c Release 
+dotnet build "NETStandard2.0/Afx.Tcp.Protocols/Afx.Tcp.Protocols.csproj" -c Release
+dotnet build "NETStandard2.1/Afx.Tcp.Protocols/Afx.Tcp.Protocols.csproj" -c Release
 cd publish
 del /q/s *.pdb
 del /q/s Afx.Sockets*
